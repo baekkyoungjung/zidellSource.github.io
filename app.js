b59735b94168f6ad0964be23bbcf6d019c39147e
baekkyoungjung <baekkyoungjung@gmail.com>
2016-12-30 15:23:27 +0900

    uueu

---
+++ app.js
@@ -0,0 +1,10 @@
+class Test extends React.Component {
+	render() {
+		return (
+			<div>
+				<div className="bonobono-hello"></div>
+			</div>
+		);
+	}
+}
+ReactDOM.render(<Test />, document.getElementById('root'));
