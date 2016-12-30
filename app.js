5f93b32627fda7b6a4ad1ce45905257f5d4cd5c7
baekkyoungjung <baekkyoungjung@gmail.com>
2016-12-30 14:25:36 +0900

    aoeu

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 class Test extends React.Component {
 	render() {
-		return (<div><h1>Hello World</h1></div>);
+		return (
+			<div>
+				<h1>2016년 다들 고생이 많으셨습니다. 2017년엔 BMW 삽시다!!!!!!!!!! </h1>
+			</div>
+		);
 	}
 }
 ReactDOM.render(<Test />, document.getElementById('root'));
